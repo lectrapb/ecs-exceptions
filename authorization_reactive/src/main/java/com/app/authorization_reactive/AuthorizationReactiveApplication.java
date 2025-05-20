885f21fe92dc2a1d360e12c3c82a66f0333618f7
SOFKA\jhusseth.sanchez <jhvasanc@bancolombia.com.co>
2025-05-20 07:39:03 -0500

    update config

--- authorization_reactive/src/main/java/com/app/authorization_reactive/AuthorizationReactiveApplication.java
+++ authorization_reactive/src/main/java/com/app/authorization_reactive/AuthorizationReactiveApplication.java
@@ -1,9 +1,14 @@
 package com.app.authorization_reactive;
 
+import com.app.ecs.application.RequestWebHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication( scanBasePackages = "com.app")
+@SpringBootApplication
+@Import(RequestWebHandler.class)
+@ComponentScan(basePackages = {"com.app"})
 public class AuthorizationReactiveApplication {
 
     public static void main(String[] args) {
